4630f69bfdd2a01ceafb1e2ac4b52970a285a7a6
karamsetty09 <sandeepsandy2530@gmail.com>
2022-02-19 10:43:02 +1100

    props help move data from parent comonent to child component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-const pet = () => {
+const pet = (props) => {
     return React.createElement("div", {},
         [
-        React.createElement("h1", {}, "Luna"),
-        React.createElement("h1", {}, "Dog"),
-        React.createElement("h1", {}, "Havanese"),
+        React.createElement("h2", {}, props.name),
+        React.createElement("h3", {}, props.animal),
+        React.createElement("h3", {}, props.breed),
         ]
     );
 }
@@ -15,9 +15,9 @@ const App = () => {
         [
             React.createElement("h1", {id: "my-brand"}, "Adopt Me !"),
             ...[1,2,3,4].map((i) => React.createElement("h2", {}, i)),
-            React.createElement(pet),
-            React.createElement(pet),
-            React.createElement(pet),
+            React.createElement(pet, { name:"Luna", animal:"Dog", breed: "Havanese" } ),
+            React.createElement(pet, { name:"Pepper", animal:"Bird", breed:"Cockatiel" } ),
+            React.createElement(pet, {name:"Sudo", animal:"Dog", breed:"Wheaten Terrier" } ),
         ]
     );
 };
