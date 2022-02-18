eabf74b27f47801196cd4540c4f4fca5784ea682
karamsetty09 <sandeepsandy2530@gmail.com>
2022-02-19 10:32:17 +1100

    creating instances based on other arother arrow functions.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+const pet = () => {
+    return React.createElement("div", {},
+        [
+        React.createElement("h1", {}, "Luna"),
+        React.createElement("h1", {}, "Dog"),
+        React.createElement("h1", {}, "Havanese"),
+        ]
+    );
+}
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {id: "my-brand"}, "Adopt Me !")
+        [
+            React.createElement("h1", {id: "my-brand"}, "Adopt Me !"),
+            ...[1,2,3,4].map((i) => React.createElement("h2", {}, i)),
+            React.createElement(pet),
+            React.createElement(pet),
+            React.createElement(pet),
+        ]
     );
 };
+
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
