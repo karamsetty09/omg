09dd8214f26a0abb19e476bd5f69cc302a27e72d
karamsetty09 <sandeepsandy2530@gmail.com>
2022-02-19 12:22:43 +1100

    component name starts with capita always.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import Pet from './Pet';
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me !"),
     ...[1, 2, 3, 4].map((i) => React.createElement("h2", {}, i)),
-    React.createElement(pet, {
+    React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
       breed: "Havanese",
     }),
-    React.createElement(pet, {
+    React.createElement(Pet, {
       name: "Pepper",
       animal: "Bird",
       breed: "Cockatiel",
     }),
-    React.createElement(pet, {
+    React.createElement(Pet, {
       name: "Sudo",
       animal: "Dog",
       breed: "Wheaten Terrier",
