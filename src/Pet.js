85cafec74db1068875995f6be10e2db7b2307294
karamsetty09 <sandeepsandy2530@gmail.com>
2022-02-19 13:22:58 +1100

    JSX and required files.

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,23 @@
-import React from 'react';
+// Both these two codes are same JSX is good for look and understanding.
+// import React from 'react';
+
+// const Pet = (props) => {
+//     return React.createElement("div", {}, [
+//       React.createElement("h2", {}, props.name),
+//       React.createElement("h3", {}, props.animal),
+//       React.createElement("h3", {}, props.breed),
+//     ]);
+//   };
 
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-      React.createElement("h2", {}, props.name),
-      React.createElement("h3", {}, props.animal),
-      React.createElement("h3", {}, props.breed),
-    ]);
-  };
+    return(
+        <div>
+            <h2>{props.name}</h2>
+            <h3>{props.name}</h3>
+            <h3>{props.name}</h3>
+        </div>
+
+    );
+};
 
 export default Pet;
