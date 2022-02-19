3fed1737465e974026cd2f42c8a4e7800397f8a4
karamsetty09 <sandeepsandy2530@gmail.com>
2022-02-19 16:10:37 +1100

    Select option display in Hooks.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,53 +1,52 @@
- import {useState} from 'react';
+ import { useState } from "react";
 
-const animals = ['dog', 'cat', 'bird', 'hens'];
+ const animals = ["dog", "cat", "bird", "hens"];
 
-const SearchParams = () => {
-    
-    // const locationTuple = useState("Seattle, WA");
-    // const location = locationTuple[0];
-    // const setLocation = locationTuple[1];
-    // Above three lines can be converted to below one line - object destructuring.
-    const [location, setLocation] = useState("Seattle, WA");
-    const [animal, setAnimal] =useState();
+ const SearchParams = () => {
+   // const locationTuple = useState("Seattle, WA");
+   // const location = locationTuple[0];
+   // const setLocation = locationTuple[1];
+   // Above three lines can be converted to below one line - object destructuring.
+   const [location, setLocation] = useState("Seattle, WA");
+   const [animal, setAnimal] = useState();
 
-    function updateLocation (event) {
-        setLocation(event.target.value);
-    };
+   function updateLocation(event) {
+     setLocation(event.target.value);
+   }
 
-    return (
-      <div className="search-params">
-        <form>
-          <label htmlFor="location">
-            Location
-            <input
-              id="location"
-              // onChange={event => setLocation(event.target.value)}
-              onChange={updateLocation}
-              value={location}
-              placeholder="Location"
-            />
-          </label>
-          <label htmlFor="animal">
-            Animals
-            <select
-              id="animal"
-              value={animal}
-              onChange={(e) => setAnimal(e.target.value)}
-              onBlur={(e) => setAnimal(e.target.value)}
-            >
-              <option value=""></option>
-              {animals.map((animal) => (
-                <option value={animal} key={animal}>
-                  {animal}
-                </option>
-              ))}
-            </select>
-          </label>
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-}
+   return (
+     <div className="search-params">
+       <form>
+         <label htmlFor="location">
+           Location
+           <input
+             id="location"
+             // onChange={event => setLocation(event.target.value)}
+             onChange={updateLocation}
+             value={location}
+             placeholder="Location"
+           />
+         </label>
+         <label htmlFor="animal">
+           Animals
+           <select
+             id="animal"
+             value={animal}
+             onChange={(e) => setAnimal(e.target.value)}
+             onBlur={(e) => setAnimal(e.target.value)}
+           >
+             <option value=""></option>
+             {animals.map((animal) => (
+               <option value={animal} key={animal}>
+                 {animal}
+               </option>
+             ))}
+           </select>
+         </label>
+         <button>Submit</button>
+       </form>
+     </div>
+   );
+ };
 
-export default SearchParams;
+ export default SearchParams;
