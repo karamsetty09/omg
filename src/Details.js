4d8a39f3e6d839ebd7d11071e148260ccf54b03b
karamsetty09 <sandeepsandy2530@gmail.com>
2022-02-20 22:42:41 +1100

    class component older than function component.

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,49 @@
-const Details = () => {
-    return <h2>Details of id</h2>
-};
+// component can be created using functions or classes.
+// this component deals with cretion of component usign class(old way).
 
-export default Details
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = { loading: true };
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await res.json();
+
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+
+    // this.setState({
+    //     loading: false,
+    //     name: json.pets[0].name,
+    //     breed: json.pets[0].breed,
+    //     animal: json.pets[0].animal,
+    // })
+  }
+  render() {
+    const {animal, breed, city, state, description, name} = this.state;
+     return (
+        <div className="details">
+            <div>
+                <h1>{name}</h1>
+                <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+                <button>Adopt {name}</button>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
+  }
+}
+export default withRouter(Details);
